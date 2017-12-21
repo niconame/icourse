6a1ace91901cbcb14d90558e136dbbc2ca9c9f9a
niconame <hazelluf@gmail.com>
2017-12-21 16:45:18 +0800

    修改 card 背景

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,23 +1,23 @@
 <template lang="html">
-  <div>
-    <!-- Tabs with card integration -->
-    <b-card no-body>
-      <b-tabs small card ref="tabs" class="mx-auto w-75 col-md-6 col-xs-12">
-        <b-tab title="Personal Information">
-          <user-info></user-info>
-        </b-tab>
-        <b-tab title="Learning Courses">
-          <user-projects></user-projects>
-        </b-tab>
-        <b-tab title="Complete Courses">
-          <user-schedule></user-schedule>
-        </b-tab>
-        <b-tab title="Notification">
-          <about-us></about-us>
-        </b-tab>
-      </b-tabs>
-    </b-card>
-  </div>
+    <div>
+      <!-- Tabs with card integration -->
+      <b-card no-body>
+        <b-tabs small card ref="tabs" class="mx-auto w-75 col-md-6 col-xs-12">
+          <b-tab title="Personal Information">
+            <user-info></user-info>
+          </b-tab>
+          <b-tab title="Learning Courses">
+            <user-projects></user-projects>
+          </b-tab>
+          <b-tab title="Complete Courses">
+            <user-schedule></user-schedule>
+          </b-tab>
+          <b-tab title="Notification">
+            <about-us></about-us>
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </div>
 </template>
 
 <script>
@@ -47,5 +47,7 @@ export default {
 </script>
 
 <style lang="css" scoped>
-
+  div.card{
+    background: linear-gradient(to left, #ece9e6, #ffffff)!important; 
+  }
 </style>
